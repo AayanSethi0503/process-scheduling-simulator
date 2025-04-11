2c69177c32a369ec724a70f5ab65c1f854cefe22
Muhammad Saif <147891557+saif010101@users.noreply.github.com>
2025-04-11 20:11:19 +0500

    started the project
    learning turtle alongside

---
+++ main.py
@@ -0,0 +1,39 @@
+import turtle as t
+
+screen = t.Screen()
+
+# set screen title
+screen.title('Process Scheduling Simulator (WIP)')
+
+# hide turtle and set speed to zero
+t.hideturtle()
+#t.speed(0)
+
+# lift the pen
+t.up()
+
+# set initial position for writing
+t.goto(100,200)
+
+# write processes text
+
+t.color('red')
+t.write('Process 1 : RED',font=('Arial',8,'bold'))
+t.sety(t.pos()[1] - 15)
+
+t.color('green')
+t.write('Process 2 : GREEN',font=('Arial',8,'bold'))
+t.sety(t.pos()[1] - 15)
+
+t.color('blue')
+t.write('Process 3 : BLUE',font=('Arial',8,'bold'))
+t.sety(t.pos()[1] - 15)
+
+t.color('black')
+t.write('Process 4 : BLACK',font=('Arial',8,'bold'))
+t.sety(t.pos()[1] - 15)
+
+t.color('purple')
+t.write('Process 5 : PURPLE',font=('Arial',8,'bold'))
+
+screen.mainloop()
